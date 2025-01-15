5274c7af74f0376759f24be7fdaf7103e4e3d45e
FDC-gabriel <125229674+FDC-gabriel@users.noreply.github.com>
2025-01-15 16:23:11 +0800

    added CORS

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 
 const routes = require('./routes')
-
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', routes)
